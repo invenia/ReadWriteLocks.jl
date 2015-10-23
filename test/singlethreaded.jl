1b5e348ee8f6a32e62755b2ce66bc2428d38af81
Eric Davies <iamed2@gmail.com>
2015-10-23 15:54:41 -0500

    More tests.

--- test/singlethreaded.jl
+++ test/singlethreaded.jl
@@ -16,34 +16,101 @@ end
 
 facts("Two-threaded tests") do
     context("Read locks") do
-        rwlock = ReadWriteLock()
-        rlock = read_lock(rwlock)
-
         NUM_LOCKS = 10
 
-        @async begin
-            @fact rwlock.readers --> 0
-            for i = 1:NUM_LOCKS
-                @fact lock!(rlock) --> nothing
-                @fact rwlock.readers --> i
+        context("$NUM_LOCKS locks") do
+            rwlock = ReadWriteLock()
+            rlock = read_lock(rwlock)
+
+            @async begin
+                @fact rwlock.writer --> false
+                @fact rwlock.readers --> 0
+                for i = 1:NUM_LOCKS
+                    @fact lock!(rlock) --> nothing
+                    @fact rwlock.readers --> i
+                end
             end
-        end
 
-        sleep(1)
+            sleep(1)
 
-        @fact rwlock.readers --> NUM_LOCKS
-
-        @async begin
             @fact rwlock.readers --> NUM_LOCKS
-            for i = NUM_LOCKS:-1:1
-                @fact unlock!(rlock) --> nothing
-                @fact rwlock.readers --> i - 1
+
+            @async begin
+                @fact rwlock.writer --> false
+                @fact rwlock.readers --> NUM_LOCKS
+                for i = NUM_LOCKS:-1:1
+                    @fact unlock!(rlock) --> nothing
+                    @fact rwlock.readers --> i - 1
+                end
+                @fact rwlock.readers --> 0
             end
+
+            sleep(1)
+
+            @fact rwlock.readers --> 0
+        end
+    end
+
+    context("Write locks") do
+        context("two locks") do
+            rwlock = ReadWriteLock()
+            wlock = write_lock(rwlock)
+
+            c = Channel{Symbol}(1)
+            put!(c, :pretest)
+
+            @async begin
+                @fact rwlock.writer --> false
+                @fact rwlock.readers --> 0
+                @fact lock!(wlock) --> nothing
+                @fact rwlock.writer --> true
+                @fact rwlock.readers --> 0
+                @fact take!(c) --> :pretest
+                put!(c, :prelock)
+                @fact lock!(wlock) --> nothing
+
+                # this code should never be reached
+                @fact take!(c) --> :prelock
+                put!(c, :postlock)
+            end
+
+            sleep(1)
+
+            @fact take!(c) --> :prelock
+            put!(c, :posttest)
+
+            @fact rwlock.writer --> true
             @fact rwlock.readers --> 0
         end
 
-        sleep(1)
+        context("unlock") do
+            rwlock = ReadWriteLock()
+            wlock = write_lock(rwlock)
 
-        @fact rwlock.readers --> 0
+            c = Channel{Symbol}(1)
+            put!(c, :pretest)
+
+            @async begin
+                @fact rwlock.writer --> false
+                @fact rwlock.readers --> 0
+                @fact lock!(wlock) --> nothing
+                @fact rwlock.writer --> true
+                @fact rwlock.readers --> 0
+                @fact take!(c) --> :pretest
+                put!(c, :preunlock)
+                @fact unlock!(wlock) --> nothing
+                @fact take!(c) --> :preunlock
+                put!(c, :postunlock)
+            end
+
+            sleep(1)
+
+            @fact take!(c) --> :postunlock
+            put!(c, :posttest)
+
+            @fact rwlock.writer --> false
+            @fact rwlock.readers --> 0
+        end
     end
+
 end
